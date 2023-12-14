a0a85babf2658448533ea38998f2c2303d45931d
kemli <kemli@DESKTOP-14FC4RN>
2023-12-14 14:35:19 +0100

    Commentaires

--- Projet Algo Poo/Joueurs.cs
+++ Projet Algo Poo/Joueurs.cs
@@ -9,98 +9,102 @@ using static System.Formats.Asn1.AsnWriter;
 namespace Projet_Algo_Poo
 {
     public class Joueurs
-    {   //creation de chaque attribut de la class joueurs
-        string Prenom;
-        List<string> MotsT = new List<string>();
-        int Score;
-
-        public Joueurs(string Prenom, int Score=0)
+    {
+        // Déclaration des attributs de la classe Joueurs
+        string Prenom; // Stocke le prénom du joueur
+        List<string> MotsT = new List<string>(); // Liste pour stocker les mots trouvés par le joueur
+        int Score; // Score du joueur
+        // Constructeur de la classe
+        public Joueurs(string Prenom, int Score = 0)
         {
-            this.Prenom = Prenom;
-            this.MotsT = new List<string>();
-            this.Score = 0;
+            this.Prenom = Prenom; // Initialisation du prénom
+            this.MotsT = new List<string>(); // Initialisation de la liste des mots trouvés
+            this.Score = 0; // Initialisation du score
         }
+        // Propriété pour accéder et modifier le prénom
         public string AppelationP
         {
             get { return this.Prenom; }
             set { this.Prenom = value; }
         }
+        // Propriété pour accéder et modifier la liste des mots trouvés
         public List<string> MotsT1
         {
             get { return this.MotsT; }
             set { this.MotsT = value; }
         }
-
+        // Propriété pour accéder et modifier le score
         public int score
         {
             get { return Score; }
             set { this.Score = value; }
         }
-
+        // Méthode pour ajouter un mot à la liste des mots trouvés
         public void Add_Mot(string mot)
         {
             // Ajoute le mot à la liste s'il n'est pas déjà présent
             if (!Contient(mot))
             {
-                MotsT.Add(mot); //C'est l'addition pour les tableaux
+                MotsT.Add(mot); // Ajout du mot à la liste
             }
         }
+        // Méthode pour vérifier si un mot est déjà dans la liste des mots trouvés
         public bool Contient(string mot)
         {
-            bool MotTrouver = false; // Réinitialise à chaque appel
+            bool MotTrouver = false; // Initialise la variable de contrôle
             foreach (var tabMot in MotsT)
             {
-                if (tabMot == mot) // Comparaison avec le mot passé en argument
+                if (tabMot == mot) // Vérifie si le mot est déjà présent
                 {
                     MotTrouver = true;
                     break; // Arrête la boucle si le mot est trouvé
                 }
             }
-            return MotTrouver;
+            return MotTrouver; // Retourne le résultat de la recherche
         }
-
+        // Méthode pour convertir les informations du joueur en chaîne de caractères
         public string toString()
         {
-            string a =":\nScore : " + this.Score + "\nMots trouvés : ";
+            string a = ":\nScore : " + this.Score + "\nMots trouvés : ";
             for (int i = 0; i < this.MotsT.Count; i++)
             {
-                a += "\n-" + this.MotsT[i];
+                a += "\n-" + this.MotsT[i]; // Ajoute chaque mot trouvé à la chaîne
             }
-            return a;
+            return a; // Retourne la chaîne de caractères complète
         }
-
+        // Méthode pour afficher le prénom avec une couleur spécifique
         public void AffichePrenom1()
         {
-            
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write(this.Prenom);
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue; // Change la couleur en bleu
+            Console.Write(this.Prenom); // Affiche le prénom
+            Console.ForegroundColor = ConsoleColor.White; // Réinitialise la couleur
         }
-
+        // Méthode pour afficher le prénom avec une autre couleur
         public void AffichePrenom2()
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write(this.Prenom);
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red; // Change la couleur en rouge
+            Console.Write(this.Prenom); // Affiche le prénom
+            Console.ForegroundColor = ConsoleColor.White; // Réinitialise la couleur
         }
-        public void Add_Score(int val) // Ajoute le score du mot au score total du joueur
+        // Méthode pour ajouter des points au score
+        public void Add_Score(int val)
         {
-            this.Score += val; //penser a definir val qui seras la valeur de chaque MotsT donc les mots trouvees par le joueur
+            this.Score += val; // Ajoute la valeur spécifiée au score actuel
         }
-
+        // Méthode pour déterminer et afficher le vainqueur entre deux joueurs
         public void Vainqueur(Joueurs joueur2)
         {
             if (this.score > joueur2.score)
             {
-                this.AffichePrenom1();
+                this.AffichePrenom1(); // Affiche le prénom du joueur actuel si son score est plus élevé
             }
             else if (this.score < joueur2.score)
             {
-                joueur2.AffichePrenom2();
+                joueur2.AffichePrenom2(); // Affiche le prénom du joueur2 si son score est plus élevé
             }
-            else if (this.score < joueur2.score)
+            else
             {
-                Console.WriteLine("Egalité !");
+                Console.WriteLine("Egalité !"); // Affiche un message d'égalité si les scores sont identiques
             }
         }
     }
--- Projet Algo Poo/Plateau.cs
+++ Projet Algo Poo/Plateau.cs
@@ -14,15 +14,11 @@ namespace Projet_Algo_Poo
     {
         public string[][] Matrice;
         public int Cote;
-        
-        
         public Plateau(string[][] Matrice, int Cote)
         {
             this.Matrice = Matrice;
             this.Cote = Cote;
-            
         }
-
         public void ToFile(string chemin)  // Sauvegarde du plateau sur un fichier externe(Sauvegarde.csv)
         {
             try
@@ -50,9 +46,7 @@ namespace Projet_Algo_Poo
                 Console.WriteLine("Le plateau n'a pas pu être sauvegardé");
                 Console.WriteLine();
             }
-            
         }
-
         public void ToRead(string chemin)  // Création du plateau a partir du fichier Test1.csv
         {
             string[] lignes = File.ReadAllLines(chemin);
@@ -61,10 +55,8 @@ namespace Projet_Algo_Poo
                 this.Matrice[i] = lignes[i].Split(';');
             }
         }
-
         public void ToReadRandom(string chemin)  // Création du plateau aléatoirement en respectant les contraintes du fichier Lettre.txt
         {
-           
             string[] lignes = File.ReadAllLines(chemin);
             string[][] transition = new string[lignes.Length][];
             string[][] informations = new string[lignes.Length][];
@@ -73,17 +65,14 @@ namespace Projet_Algo_Poo
                 transition[i] = lignes[i].Split(';');
                 informations[i] = transition[i][0].Split(',');
             }
-
             for (int i = 0;i < this.Cote; i++)
             {
                 this.Matrice[i] = new string[this.Cote];
-                
             }
             int[] compteur = new int[26];
             Random r = new Random();
             for (int i = 0; i < this.Cote ; i++)
             {
-                
                 for (int j = 0; j < this.Cote; j++)
                 {
                     int element = r.Next(0, 26);
@@ -107,14 +96,12 @@ namespace Projet_Algo_Poo
                 }
                 chainematrice += "\n";
             }
-
             return chainematrice;
         }
 
         public (bool,string[][]) Recherche_Mot(string mot)  // recherche si le mot entré est present dans le plateau selon les règles 
         {
             char[] lettres = mot.ToCharArray();
-            
             Stack<int[]> indices = new Stack<int[]>();
             List<int[]> interdits = new List<int[]>();
             int[] indicelettres = new int[2];
@@ -127,31 +114,23 @@ namespace Projet_Algo_Poo
                     int[] indtransi = new int [2] {this.Cote-1,i};
                     indices.Push (indtransi);
                     p = true;
-                    
                 }
             }
             if (p == true)
             {
                 (bool present, Stack<int[]> indicesfinal) = Recherche(lettres, indices, interdits, comptelettre);
-                
                 if (present == true)
                 {
                     int taille = indicesfinal.Count;
                     for (int i = 0; i < taille; i++)
                     {
                         int[] indicesup = indicesfinal.Pop();
-                        
                         this.Matrice[indicesup[0]][indicesup[1]] = " ";
-                        
-                        
                     }
                     return (true, this.Matrice);
                 }
             }
-            
             return (false, this.Matrice);
-
-
         }
 
         public (bool, Stack<int[]>) Recherche(char[] lettres, Stack<int[]> indices, List<int[]> interdits,int comptelettre) // Recherche
@@ -160,19 +139,16 @@ namespace Projet_Algo_Poo
             {
                 return (true,indices);
             }
-
             else if (comptelettre == 1 && PresenceGauche(lettres,indices,interdits,comptelettre) == false && PresenceDroite(lettres, indices, interdits, comptelettre) == false && PresenceHaut(lettres, indices, interdits, comptelettre) == false && PresenceHautGauche(lettres, indices, interdits, comptelettre) == false && PresenceHautDroite(lettres, indices, interdits, comptelettre) == false)
             {
                 return (false,indices);
             }
-
             else if (comptelettre > 1 && PresenceGauche(lettres, indices, interdits, comptelettre) == false && PresenceDroite(lettres, indices, interdits, comptelettre) == false && PresenceHaut(lettres, indices, interdits, comptelettre) == false && PresenceHautGauche(lettres, indices, interdits, comptelettre) == false && PresenceHautDroite(lettres, indices, interdits, comptelettre) == false)
             {
                 int[] transiinterdits = indices.Pop();
                 interdits.Add(transiinterdits);
                 return Recherche(lettres, indices, interdits, comptelettre);
             }
-
             else if (PresenceGauche(lettres, indices, interdits, comptelettre) == true)
             {
                 int[] ind = indices.Peek();
@@ -183,7 +159,6 @@ namespace Projet_Algo_Poo
                 comptelettre++;
                 return Recherche(lettres,indices,interdits,comptelettre);
             }
-
             else if (PresenceHautGauche(lettres, indices, interdits, comptelettre) == true)
             {
                 int[] ind = indices.Peek();
@@ -194,7 +169,6 @@ namespace Projet_Algo_Poo
                 comptelettre++;
                 return Recherche(lettres, indices, interdits, comptelettre);
             }
-
             else if (PresenceHaut(lettres, indices, interdits, comptelettre) == true)
             {
                 int[] ind = indices.Peek();
@@ -205,7 +179,6 @@ namespace Projet_Algo_Poo
                 comptelettre++;
                 return Recherche(lettres, indices, interdits, comptelettre);
             }
-
             else if (PresenceHautDroite(lettres, indices, interdits, comptelettre) == true)
             {
                 int[] ind = indices.Peek();
@@ -216,7 +189,6 @@ namespace Projet_Algo_Poo
                 comptelettre++;
                 return Recherche(lettres, indices, interdits, comptelettre);
             }
-
             else if (PresenceDroite(lettres, indices, interdits, comptelettre) == true)
             {
                 int[] ind = indices.Peek();
@@ -233,12 +205,10 @@ namespace Projet_Algo_Poo
         public bool PresenceGauche (char[] lettres, Stack<int[]> indices, List<int[]> interdits, int comptelettre)
         {
             int[] ind = indices.Peek();
-            
             if (ind[1] == 0)
             {
                 return false;
             }
-            
             for (int i = 0; i < interdits.Count; i++)
             {
                 if (interdits[i] == ind)
@@ -246,10 +216,8 @@ namespace Projet_Algo_Poo
                     return false;
                 }
             }
-            
             if (this.Matrice[ind[0]][ind[1]-1] == Convert.ToString(lettres[comptelettre]))
             {
-                
                 return true;
             }
             return false;
@@ -258,7 +226,6 @@ namespace Projet_Algo_Poo
         public bool PresenceDroite(char[] lettres, Stack<int[]> indices, List<int[]> interdits, int comptelettre)
         {
             int[] ind = indices.Peek();
-            
             for (int i = 0; i < interdits.Count; i++)
             {
                 if (interdits[i] == ind)
@@ -266,16 +233,12 @@ namespace Projet_Algo_Poo
                     return false;
                 }
             }
-            
             if (ind[1] == this.Cote-1)
             {
                 return false;
             }
-            
             else if (this.Matrice[ind[0]][ind[1] + 1] == Convert.ToString(lettres[comptelettre]))
             {
-
-                
                 return true;
             }
             return false;
@@ -314,7 +277,6 @@ namespace Projet_Algo_Poo
             {
                 return false;
             }
-
             for (int i = 0; i < interdits.Count; i++)
             {
                 if (interdits[i] == ind)
@@ -322,7 +284,6 @@ namespace Projet_Algo_Poo
                     return false;
                 }
             }
-
             if (this.Matrice[ind[0] - 1][ind[1] - 1] == Convert.ToString(lettres[comptelettre]))
             {
                 
@@ -339,7 +300,6 @@ namespace Projet_Algo_Poo
             {
                 return false;
             }
-
             for (int i = 0; i < interdits.Count; i++)
             {
                 if (interdits[i] == ind)
@@ -347,7 +307,6 @@ namespace Projet_Algo_Poo
                     return false;
                 }
             }
-
             if (this.Matrice[ind[0] - 1][ind[1] + 1] == Convert.ToString(lettres[comptelettre]))
             {
                 
